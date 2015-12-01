27e0249cf553060a1c87d57a4797bea64048ad17
Iris <iris.mehrbrodt@gmx.de>
2015-12-01 16:31:06 +0100

    dialog style

--- app/src/main/java/edu/kit/teco/radarbeacon/SelectDeviceDialog.java
+++ app/src/main/java/edu/kit/teco/radarbeacon/SelectDeviceDialog.java
@@ -1,12 +1,12 @@
 package edu.kit.teco.radarbeacon;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -94,7 +94,7 @@ public class SelectDeviceDialog extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.CustomDialogTheme);
         LayoutInflater inflater = getActivity().getLayoutInflater();
         adapter = new CustomAdapter();
 
